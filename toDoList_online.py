50eb6c718449c8ab27c67d62a8ad770ab8d648ba
MahmoudMADIH <moodbeder150@gmail.com>
2023-08-21 18:10:46 +0300

    adding

--- toDoList_online.py
+++ toDoList_online.py
@@ -54,8 +54,6 @@ def retrieve_database():
         tasks.pop()
 
 
-
-
 if __name__ == "__main__":
     guiWindow = Tk()
     guiWindow.title("To-Do List ")
@@ -73,68 +71,57 @@ if __name__ == "__main__":
 
     functions_frame.pack(side="top", expand=True, fill="both")
 
-    task_label = Label(functions_frame, text="Enter the Task:",
-                       font=("arial", "14", "bold"),
+    task_label = Label(functions_frame, text="Enter the task",
+                       font=("arial", 14, "bold"),
                        background="black",
-                       foreground="white"
-                       )
+                       foreground="white")
     task_label.place(x=20, y=30)
 
-    task_field = Entry(
-        functions_frame,
-        font=("Arial", "14"),
-        width=42,
-        foreground="black",
-        background="white",
-    )
+    task_field = Entry(functions_frame,
+                       font=("arial", 14),
+                       width=42,
+                       background="white",
+                       foreground="black")
     task_field.place(x=180, y=30)
 
-    add_button = Button(
-        functions_frame,
-        text="Add Task",
-        width=15,
-        bg='#D4AC0D', font=("arial", "14", "bold"),
-        command=add_task,
-
-    )
-    del_button = Button(
-        functions_frame,
-        text="Delete Task",
-        width=15,
-        bg='#D4AC0D', font=("arial", "14", "bold"),
-        command=delete_task,
-    )
-    del_all_button = Button(
-        functions_frame,
-        text="Delete All Tasks",
-        width=15,
-        font=("arial", "14", "bold"),
-        bg='#D4AC0D',
-        command=delete_all_tasks
-    )
-    exit_button = Button(
-        functions_frame,
-        text="Exit",
-        width=52,
-        bg='#D4AC0D', font=("arial", "14", "bold"),
-        command=close
-    )
-    add_button.place(x=18, y=80, )
+    add_button = Button(functions_frame,
+                        font=("arial", 14, "bold"),
+                        width=15,
+                        text="add task",
+                        command=add_task,
+                        bg='#D4AC0D')
+    del_button = Button(functions_frame,
+                        font=("arial", 14, "bold"),
+                        width=15,
+                        bg='#D4AC0D',
+                        text="delete_task",
+                        command=delete_task)
+    del__all_button = Button(functions_frame,
+                             font=("arial", 14, "bold"),
+                             width=15,
+                             bg='#D4AC0D',
+                             text="delete All task",
+                             command=delete_all_tasks)
+    exit_button = Button(functions_frame,
+                         font=("arial", 14, "bold"),
+                         width=52,
+                         bg='#D4AC0D',
+                         text="Exit",
+                         command=close)
+    add_button.place(x=18, y=80)
     del_button.place(x=240, y=80)
-    del_all_button.place(x=460, y=80)
+    del__all_button.place(x=460, y=80)
     exit_button.place(x=17, y=330)
 
-    task_listbox = Listbox(
-        functions_frame,
-        width=57,
-        height=7,
-        font="bold",
-        selectmode='SINGLE',
-        background="WHITE",
-        foreground="BLACK",
-        selectbackground="#D4AC0D",
-        selectforeground="BLACK"
-    )
+    task_listbox = Listbox(functions_frame,
+                           font=('bold'),
+                           width=57,
+                           height=7,
+                           selectmode='SINGEL',
+                           background='WHITE',
+                           foreground='BLACK',
+                           selectbackground='#D4AC0D',
+                           selectforeground='WHITE')
     task_listbox.place(x=17, y=140)
 
     retrieve_database()
